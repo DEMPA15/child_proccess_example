f737f3cb96244bdfbbb19b56fa2df18d6cf5afd9
Joshua Richard Smith <veryjoshua@gmail.com>
2018-05-31 22:35:59 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { spawn } = require('child_process');
+
+const ls = spawn('mocha', ['index.test.js']);
+
+ls.stdout.on('data', (data) => {
+    console.log(`stdout: ${data}`);
+  });
+  
+  ls.stderr.on('data', (data) => {
+    console.log(`stderr: ${data}`);
+  });
+  
+  ls.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+  });
---
+++ index.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('Testing Reverse a number',()=>{
+
+    it('is a function', function(){
+        expect(reverseNum).to.be.a('function');
+    })
+
+});
